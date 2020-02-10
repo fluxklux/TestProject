e9b68f0b8c57ddb5576790ac0435e2603c426802
Carl Eriksson <carl.erikssonn@hotmail.com>
2020-02-10 11:57:50 +0100

    Added first version of dynamic controller system (add players in start menu)

--- Test/Assets/InputController.cs
+++ Test/Assets/InputController.cs
@@ -5,20 +5,31 @@ using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
+    //make private later, public for debugging reasons
+    public int playerCount = 0;
     public bool[] hasPressedKey = { false, false, false, false };
+    public bool[] hasJoined = { false, false, false, false };
+    public int[] playerArray = { 0, 1, 2, 3 };
+
+    public GameObject[] allPlayers = { };
+
     [Header("DEBUG")]
     public GUIStyle style;
-
-    //private int playersCount = 0;
+    
     private GameController gc;
     private DPad dc;
+    private UIController uc;
+    private MoveController mc;
+
     private bool canTakeInput = true;
-    private int playerCount = 0;
+    private bool gameStarted = false;
 
     void Start()
     {
+        mc = GetComponent<MoveController>();
         dc = GetComponent<DPad>();
         gc = GetComponent<GameController>();
+        uc = GetComponent<UIController>();
     }
 
     public void ChangeTakeInputBool (bool onOff)
@@ -26,35 +37,107 @@ public class InputController : MonoBehaviour
         canTakeInput = onOff;
     }
 
-    public void AddPlayer ()
+    public void AddPlayer (int index)
     {
+        Debug.Log("Entered AddPlayer Function");
+        if(playerCount < 4)
+        {
+            //add events
+            playerCount++;
+            hasJoined[index] = true;
+            uc.TogglePlayerUi(index, true);
 
+            //add mechanical functions
+            //KAN INTE DYNAMISKT LÄGGA TILL OCH TA BORT SPELAR OBJEKTEN FÖR DEM LIGGER-
+            //I GAMECONTROLLER OCH JAG MÅSTE ÄNDRA SPELAR ARRAYEN TILL EN LIST. BEHÖVER-
+            //VIKBERG I SAMMA RUM FÖR DET. SÅ GITEN INTE FÅR MERGE-FEL.
+        }
+        else
+        {
+            Debug.Log("max player count reached!");
+        }
+    }
+
+    private void StartGame ()
+    {
+        gameStarted = true;
+        uc.ToggleConnectionUi(false);
+        Debug.Log("Started game!");
     }
 
     private void Update()
     {
+        //add controllers
+        if(!gameStarted)
+        {
+            //Start Game
+            if(playerCount >= 2)
+            {
+                if(Input.GetButtonDown("C1 Start"))
+                {
+                    StartGame();
+                }
+
+                if (Input.GetButtonDown("C2 Start"))
+                {
+                    StartGame();
+                }
+
+                if(Input.GetButtonDown("C3 Start"))
+                {
+                    StartGame();
+                }
+            }
+
+            //Connect players
+            if(Input.GetButtonDown("C1 Select") && !hasJoined[0])
+            {
+                Debug.Log("CONTROLLER 1");
+                AddPlayer(0);
+            }
+
+            if (Input.GetButtonDown("C2 Select") && !hasJoined[1])
+            {
+                Debug.Log("CONTROLELR 2");
+                AddPlayer(1);
+            }
+
+            if (Input.GetButtonDown("C3 Select") && !hasJoined[2])
+            {
+                Debug.Log("CONTROLELR 3");
+                AddPlayer(2);
+            }
+        }
+
+        //take input mid game
         if (!gc.queueFinished)
         {
             if (canTakeInput)
             {
                 //för plebsen utan xbox controllers. lmao
                 switch (Input.inputString)
-                    {
-                        case "1":
-                            if (!hasPressedKey[0])
-                            {
-                                gc.HandleQueueInputs(0, 0);
-                            }
-                            break;
-                        case "2":
-                            if (!hasPressedKey[1])
-                            {
-                                gc.HandleQueueInputs(1, 0);
-                            }
-                            break;
-                        default:
-                            break;
-                    }
+                {
+                    case "1":
+                        if (!hasPressedKey[0])
+                        {
+                            gc.HandleQueueInputs(0, 0);
+                        }
+                        break;
+                    case "2":
+                        if (!hasPressedKey[1])
+                        {
+                            gc.HandleQueueInputs(1, 0);
+                        }
+                        break;
+                    case "3":
+                        if(!hasPressedKey[2])
+                        {
+                            gc.HandleQueueInputs(2, 0);
+                        }
+                        break;
+                    default:
+                        break;
+                }
 
                 //p1
                 if (playerCount > 0 && !hasPressedKey[0])
@@ -117,6 +200,37 @@ public class InputController : MonoBehaviour
                             break;
                     }
                 }
+
+                //p3
+                if (playerCount > 2 && !hasPressedKey[2])
+                {
+                    var c3Horizontal = Input.GetAxis("C3 Horizontal");
+                    var c3Vertical = Input.GetAxis("C3 Vertical");
+
+                    switch (c3Horizontal)
+                    {
+                        case 1:
+                            gc.HandleQueueInputs(2, 1); //right
+                            break;
+                        case -1:
+                            gc.HandleQueueInputs(2, 3);//left
+                            break;
+                        default:
+                            break;
+                    }
+
+                    switch (c3Vertical)
+                    {
+                        case 1:
+                            gc.HandleQueueInputs(2, 0); //up
+                            break;
+                        case -1:
+                            gc.HandleQueueInputs(2, 2);//down
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         }
     }
@@ -125,12 +239,15 @@ public class InputController : MonoBehaviour
     {
         var c1Hor = Input.GetAxis("C1 Horizontal");
         var c2Hor = Input.GetAxis("C2 Horizontal");
+        var c3Hor = Input.GetAxis("C3 Horizontal");
 
         var c1Ver = Input.GetAxis("C1 Vertical");
         var c2Ver = Input.GetAxis("C2 Vertical");
+        var c3Ver = Input.GetAxis("C3 Vertical");
 
         string debugString = "P1: " + c1Hor.ToString("F2") + ", " + c1Ver.ToString("F2")
-            + "\n" + "P2: " + c2Hor.ToString("F2") + ", " + c2Ver.ToString("F2");
+            + "\n" + "P2: " + c2Hor.ToString("F2") + ", " + c2Ver.ToString("F2")
+            + "\n" + "P3: " + c3Hor.ToString("F2") + ", " + c3Ver.ToString("F2");
 
         GUI.Label(new Rect(10, 10, 250, 200), debugString, style);
     }
--- Test/Assets/UIController.cs
+++ Test/Assets/UIController.cs
@@ -6,24 +6,38 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [Header("Player Texts")]
-
-    public Text[] playerTexts;
+    public GameObject[] playerTexts;
+    public GameObject[] connectionPanelsNotJoined;//vidrigt, fixa längre fram
+    public GameObject[] connectionPanelsJoined;//vidrigt, fixa längre fram
+    public GameObject connectionPanel;
 
     public Text timerText;
     public Text eventText;
 
-    GameController gc;
+    private GameController gc;
 
-    void Start()
+    private void Start()
     {
         gc = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+
+    public void TogglePlayerUi (int index, bool onOff)
+    {
+        playerTexts[index].SetActive(onOff);
+        connectionPanelsNotJoined[index].SetActive(false);
+        connectionPanelsJoined[index].SetActive(true);
+    }
+
+    public void ToggleConnectionUi (bool onOff)
+    {
+        connectionPanel.SetActive(onOff);
     }
 
     public void UpdatePlayerFruits(int[] playerFruits)
     {
         for (int i = 0; i < playerTexts.Length; i++)
         {
-            playerTexts[i].text = "P" + i + ": " + playerFruits[i];
+            playerTexts[i].GetComponentInChildren<Text>().text = "P" + i + ": " + playerFruits[i];
         }
     }
 
